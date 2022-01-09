7ac99c0cb3e37fc26a4bf95c705f25bf8e8044b7
BlankRip <aniketh_g5@outlook.com>
2022-01-09 20:10:41 +0530

    It all works now

--- Assets/Scripts/JustForTesting/TestEnemy.cs
+++ Assets/Scripts/JustForTesting/TestEnemy.cs
@@ -9,10 +9,10 @@ namespace JustTesting {
     {
         [SerializeField] float speed;
         [SerializeField] float flipAfter;
+        private Vector3 refPoint;
 
         [Header("Saving related things")]
         [SerializeField] int objectId;
-        private Vector3 refPoint;
 
         private void Start() {
             refPoint = transform.position;
@@ -30,12 +30,13 @@ namespace JustTesting {
         }
 
         public string GetDataToSave() {
-            MySaveData dataToSend = new MySaveData(transform.position, refPoint);
+            MySaveData dataToSend = new MySaveData(transform.position, refPoint, speed);
             return JsonUtility.ToJson(dataToSend);
         }
 
         public void Load(string recievedData) {
             MySaveData data = JsonUtility.FromJson<MySaveData>(recievedData);
+            speed = data.speed;
             transform.position = data.position;
             refPoint = data.referencePoint;
         }
@@ -52,10 +53,12 @@ namespace JustTesting {
         private class MySaveData {
             public Vector3 position;
             public Vector3 referencePoint;
+            public float speed;
 
-            public MySaveData(Vector3 position, Vector3 refPoint) {
+            public MySaveData(Vector3 position, Vector3 refPoint, float speed) {
                 this.position = position;
                 this.referencePoint = refPoint;
+                this.speed = speed;
             }
         }
     }
--- Assets/Scripts/JustForTesting/TestLightMeter.cs
+++ Assets/Scripts/JustForTesting/TestLightMeter.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using SaveSystem;
 
 namespace JustTesting {
-    public class TestLightMeter : MonoBehaviour
+    public class TestLightMeter : MonoBehaviour, ISavable
     {
         private Image testImg;
-        
+
+        [Header("Saving related things")]
+        [SerializeField] int objectId;
+
         private void Start() {
+            SaveManager.instance.AddSavable(this);
             testImg = GetComponent<Image>();
         }
 
@@ -17,6 +22,33 @@ namespace JustTesting {
                 testImg.fillAmount += .1f;
             else if(Input.GetKeyDown(KeyCode.Q))
                 testImg.fillAmount -= .1f;
+        }
+
+        public string GetDataToSave() {
+            string dataToSend = JsonUtility.ToJson(new MySaveData(testImg.fillAmount));
+            return dataToSend;
+        }
+
+        public void Load(string recievedData) {
+            MySaveData data = JsonUtility.FromJson<MySaveData>(recievedData);
+            testImg.fillAmount = data.fillAmount;
+        }
+
+        public int GetItemId() {
+            return objectId;
+        }
+
+        public GameObject GetGameObject() {
+            return this.gameObject;
+        }
+
+        [System.Serializable]
+        private class MySaveData {
+            public float fillAmount;
+
+            public MySaveData(float fillAmount) {
+                this.fillAmount = fillAmount;
+            }
         }
     }
 }
